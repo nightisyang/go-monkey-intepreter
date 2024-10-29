4b5749907160b985d5e979875d331663b1864a4a
nightisyang <shenyangc86@gmail.com>
2024-10-29 14:11:46 +0800

    add evaluation for string literal

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"lexer/evaluator"
 	"lexer/lexer"
 	"lexer/parser"
 )
@@ -45,8 +46,13 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
